fb5982a8f1fce5807c129e9b545e336d353e7803
Open Panzer <admin@openpanzer.org>
2018-07-07 16:32:04 -0500

    Remove unnecessary help file, update batch file that creates .qhc and .qch to auto-copy to compile folder for ease of internal testing.

--- help_files/createqhcp_5.4.bat
+++ help_files/createqhcp_5.4.bat
@@ -1,1 +1,5 @@
 C:\Qt\5.4\mingw491_32\bin\qcollectiongenerator.exe opconfighelp.qhcp -o opconfighelp.qhc
+
+xcopy /s /y D:\UAV\QT\Projects\OpenPanzerConfig\help_files\opconfighelp.qch D:\UAV\QT\Projects\build-OpenPanzerConfig-Desktop_Qt_5_4_2_MinGW_32bit-Release\release\assistant\help_files\opconfighelp.qch
+
+xcopy /s /y D:\UAV\QT\Projects\OpenPanzerConfig\help_files\opconfighelp.qhc D:\UAV\QT\Projects\build-OpenPanzerConfig-Desktop_Qt_5_4_2_MinGW_32bit-Release\release\assistant\help_files\opconfighelp.qhc

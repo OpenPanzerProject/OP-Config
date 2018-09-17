b1cdc33ceb82221034b4177320fb8d59edebe9d5
Open Panzer <admin@openpanzer.org>
2018-09-16 20:45:13 -0500

    Update to Version 0.93.41

--- version.h
+++ version.h
@@ -1,18 +1,18 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-// LAST UPDATE: 7/01/2018
+// LAST UPDATE: 9/16/2018
 
-#define VER_FILEVERSION              0,93,07,00
-#define VER_FILEVERSION_STR         "0.93.07.00\0"
+#define VER_FILEVERSION              0,93,41,00
+#define VER_FILEVERSION_STR         "0.93.41.00\0"
 
 // THIS IS THE VERSION THAT WILL BE CHECKED AGAINST the appcast.xml version on the downloads page.
 // YOU MUST ALSO RUN QMAKE for this to update.
-#define VER_PRODUCTVERSION           0,93,07,00
-#define VER_PRODUCTVERSION_STR      "0.93.07\0"
+#define VER_PRODUCTVERSION           0,93,41,00
+#define VER_PRODUCTVERSION_STR      "0.93.41\0"
 
 // THIS VERSION IS THE MINIMUM VERSION OF TCB FIRMWARE NEEDED FOR THIS VERSION OF OP CONFIG:
-#define VER_MINTCB_STR              "0.93.07\0"
+#define VER_MINTCB_STR              "0.93.41\0"
 
 #define VER_COPYRIGHT_YEAR_STR      "2018"      // Change it below as well v
 #define VER_COMPANYNAME_STR         "OpenPanzer.org"

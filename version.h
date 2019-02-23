ba1f49a100385bbef186b3cab8ca567708bba474
Open Panzer <admin@openpanzer.org>
2019-02-23 16:44:16 -0600

    Update to Version 0.93.45

--- version.h
+++ version.h
@@ -1,24 +1,24 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-// LAST UPDATE: 12/01/2018
+// LAST UPDATE: 2/23/2019
 
-#define VER_FILEVERSION              0,93,42,00
-#define VER_FILEVERSION_STR         "0.93.42.00\0"
+#define VER_FILEVERSION              0,93,45,00
+#define VER_FILEVERSION_STR         "0.93.45.00\0"
 
 // THIS IS THE VERSION THAT WILL BE CHECKED AGAINST the appcast.xml version on the downloads page.
 // YOU MUST ALSO RUN QMAKE for this to update.
-#define VER_PRODUCTVERSION           0,93,42,00
-#define VER_PRODUCTVERSION_STR      "0.93.42\0"
+#define VER_PRODUCTVERSION           0,93,45,00
+#define VER_PRODUCTVERSION_STR      "0.93.45\0"
 
 // THIS VERSION IS THE MINIMUM VERSION OF TCB FIRMWARE NEEDED FOR THIS VERSION OF OP CONFIG:
-#define VER_MINTCB_STR              "0.93.41\0"
+#define VER_MINTCB_STR              "0.93.45\0"
 
-#define VER_COPYRIGHT_YEAR_STR      "2018"      // Change it below as well v
+#define VER_COPYRIGHT_YEAR_STR      "2019"      // Change it below as well v
 #define VER_COMPANYNAME_STR         "OpenPanzer.org"
 #define VER_FILEDESCRIPTION_STR     "OP Config"
 #define VER_INTERNALNAME_STR        "OP Config"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright (C) 2018 OpenPanzer.org"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (C) 2019 OpenPanzer.org"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "OPConfig.exe"

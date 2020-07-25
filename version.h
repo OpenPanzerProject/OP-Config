2db1df5456ab624a7e2dda99918c2b2cedb2f77a
Open Panzer <admin@openpanzer.org>
2020-07-25 21:26:54 +0100

    Update to Version 0.93.69

--- version.h
+++ version.h
@@ -1,15 +1,15 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-// LAST UPDATE: 1/27/2020
+// LAST UPDATE: 7/25/2020
 
-#define VER_FILEVERSION              0,93,68,00
-#define VER_FILEVERSION_STR         "0.93.68.00\0"
+#define VER_FILEVERSION              0,93,69,00
+#define VER_FILEVERSION_STR         "0.93.69.00\0"
 
 // THIS IS THE VERSION THAT WILL BE CHECKED AGAINST the appcast.xml version on the downloads page.
 // YOU MUST ALSO RUN QMAKE for this to update.
-#define VER_PRODUCTVERSION           0,93,68,00
-#define VER_PRODUCTVERSION_STR      "0.93.68\0"
+#define VER_PRODUCTVERSION           0,93,69,00
+#define VER_PRODUCTVERSION_STR      "0.93.69\0"
 
 // THIS VERSION IS THE MINIMUM VERSION OF DEVICE FIRMWARE NEEDED FOR THIS VERSION OF OP CONFIG:
 #define VER_MINTCB_STR              "0.93.68\0"
